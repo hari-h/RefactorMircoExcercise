b3b6c3f0db9e16b8c089a8d4596a1221700e2a9c
Haris H <haris@mss.ba>
2022-09-13 14:53:06 +0200

    - Added initial AlarmTest.cs - new NUnit packages because the provided library files didn't working properly - Dummy test is working, NUnit working

---
+++ src/TirePressureMonitoringSystem.Test/AlarmTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TDDMicroExercises.TirePressureMonitoringSystem.Test
+{
+    /// <summary>
+    /// 1. TirePressureMonitoringSystem exercise:
+    /// Write the unit tests for the Alarm class, refactor the code as much as you need to make the class testable.
+    /// The Alarm class is designed to monitor tire pressure and set an alarm if the pressure falls outside of the expected range. 
+    /// The Sensor class provided for the exercise simulates the behaviour of a real tire sensor, providing random but realistic values.
+    /// </summary>
+    [TestFixture]
+    public class AlarmTest
+    {
+        [Test]
+        public void CheckPressure_low_treshold_should_turn_on_alarm()
+        {
+            Alarm anAlarm = new Alarm();
+            anAlarm.Check();
+            bool isAlarmOn = anAlarm.AlarmOn;
+            Assert.IsTrue(isAlarmOn);
+        }
+    }
+}
